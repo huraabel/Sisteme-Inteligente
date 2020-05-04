3524b06eacff485a83116b729da403a2fbfed0ab
huraabel <huraabeljonathan@gmail.com>
2020-05-04 18:44:48 +0300

    Add a Neuron to hidden layer

--- ML/complete_nn.py
+++ ML/complete_nn.py
@@ -45,16 +45,24 @@ class OurNeuralNetwork:
     self.w5 = np.random.normal()
     self.w6 = np.random.normal()
 
+    self.w7 = np.random.normal()
+    self.w8 = np.random.normal()
+    self.w9 = np.random.normal()
+
+
     # Biases
     self.b1 = np.random.normal()
     self.b2 = np.random.normal()
     self.b3 = np.random.normal()
 
+    self.b4 = np.random.normal()
+
   def feedforward(self, x):
     # x is a numpy array with 2 elements.
     h1 = self.activation_f(self.w1 * x[0] + self.w2 * x[1] + self.b1)
     h2 = self.activation_f(self.w3 * x[0] + self.w4 * x[1] + self.b2)
-    o1 = self.activation_f(self.w5 * h1 + self.w6 * h2 + self.b3)
+    h3 = self.activation_f(self.w7 * x[0] + self.w8 * x[1] + self.b4)
+    o1 = self.activation_f(self.w5 * h1 + self.w6 * h2 + self.w9 * h3 + self.b3)
     return o1
 
   def train(self, data, all_y_trues):
@@ -75,7 +83,10 @@ class OurNeuralNetwork:
         sum_h2 = self.w3 * x[0] + self.w4 * x[1] + self.b2
         h2 = self.activation_f(sum_h2)
 
-        sum_o1 = self.w5 * h1 + self.w6 * h2 + self.b3
+        sum_h3 = self.w7 * x[0] + self.w8 * x[1] + self.b4  
+        h3 = self.activation_f(sum_h3)
+
+        sum_o1 = self.w5 * h1 + self.w6 * h2 + self.w9 * h3 + self.b3
         o1 = self.activation_f(sum_o1)
         y_pred = o1
 
@@ -86,10 +97,14 @@ class OurNeuralNetwork:
         # Neuron o1
         d_ypred_d_w5 = h1 * self.deriv_activation_f(sum_o1)
         d_ypred_d_w6 = h2 * self.deriv_activation_f(sum_o1)
+        d_ypred_d_w9 = h3 * self.deriv_activation_f(sum_o1)
+
         d_ypred_d_b3 = self.deriv_activation_f(sum_o1)
 
+        
         d_ypred_d_h1 = self.w5 * self.deriv_activation_f(sum_o1)
         d_ypred_d_h2 = self.w6 * self.deriv_activation_f(sum_o1)
+        d_ypred_d_h3 = self.w9 * self.deriv_activation_f(sum_o1)
 
         # Neuron h1
         d_h1_d_w1 = x[0] * self.deriv_activation_f(sum_h1)
@@ -100,6 +115,12 @@ class OurNeuralNetwork:
         d_h2_d_w3 = x[0] * self.deriv_activation_f(sum_h2)
         d_h2_d_w4 = x[1] * self.deriv_activation_f(sum_h2)
         d_h2_d_b2 = self.deriv_activation_f(sum_h2)
+
+        #Neuron h3
+        d_h3_d_w7 = x[0] * self.deriv_activation_f(sum_h3)
+        d_h3_d_w8 = x[1] * self.deriv_activation_f(sum_h3)
+        d_h3_d_b4 = self.deriv_activation_f(sum_h3)
+
 
         # --- Update weights and biases
         # Neuron h1
@@ -112,9 +133,15 @@ class OurNeuralNetwork:
         self.w4 -= learn_rate * d_L_d_ypred * d_ypred_d_h2 * d_h2_d_w4
         self.b2 -= learn_rate * d_L_d_ypred * d_ypred_d_h2 * d_h2_d_b2
 
+        # Neuron h3
+        self.w7 -= learn_rate * d_L_d_ypred * d_ypred_d_h3 * d_h3_d_w7
+        self.w8 -= learn_rate * d_L_d_ypred * d_ypred_d_h3 * d_h3_d_w8
+        self.b4 -= learn_rate * d_L_d_ypred * d_ypred_d_h3 * d_h3_d_b4
+
         # Neuron o1
         self.w5 -= learn_rate * d_L_d_ypred * d_ypred_d_w5
         self.w6 -= learn_rate * d_L_d_ypred * d_ypred_d_w6
+        self.w9 -= learn_rate * d_L_d_ypred * d_ypred_d_w9
         self.b3 -= learn_rate * d_L_d_ypred * d_ypred_d_b3
 
       # --- Calculate total loss at the end of each epoch
