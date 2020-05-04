0510d91e6c81c1c24c1a91cd25ee4381cad9eb4b
huraabel <huraabeljonathan@gmail.com>
2020-05-04 17:37:39 +0300

    implement ReLu fucntion, add generic activation function as parameter of class

--- ML/complete_nn.py
+++ ML/complete_nn.py
@@ -9,6 +9,13 @@ def deriv_sigmoid(x):
   fx = sigmoid(x)
   return fx * (1 - fx)
 
+def ReLu(x):
+    return max(0, x)
+
+def deriv_ReLu(x):
+    return 1
+
+
 def mse_loss(y_true, y_pred):
   # y_true and y_pred are numpy arrays of the same length.
   return ((y_true - y_pred) ** 2).mean()
@@ -25,7 +32,11 @@ class OurNeuralNetwork:
   Real neural net code looks nothing like this. DO NOT use this code.
   Instead, read/run it to understand how this specific network works.
   '''
-  def __init__(self):
+  def __init__(self, activation_function):
+    
+    self.activation_f = activation_function
+    self.deriv_activation_f = eval("deriv_" + activation_function.__name__)
+
     # Weights
     self.w1 = np.random.normal()
     self.w2 = np.random.normal()
@@ -41,9 +52,9 @@ class OurNeuralNetwork:
 
   def feedforward(self, x):
     # x is a numpy array with 2 elements.
-    h1 = sigmoid(self.w1 * x[0] + self.w2 * x[1] + self.b1)
-    h2 = sigmoid(self.w3 * x[0] + self.w4 * x[1] + self.b2)
-    o1 = sigmoid(self.w5 * h1 + self.w6 * h2 + self.b3)
+    h1 = self.activation_f(self.w1 * x[0] + self.w2 * x[1] + self.b1)
+    h2 = self.activation_f(self.w3 * x[0] + self.w4 * x[1] + self.b2)
+    o1 = self.activation_f(self.w5 * h1 + self.w6 * h2 + self.b3)
     return o1
 
   def train(self, data, all_y_trues):
@@ -59,13 +70,13 @@ class OurNeuralNetwork:
       for x, y_true in zip(data, all_y_trues):
         # --- Do a feedforward (we'll need these values later)
         sum_h1 = self.w1 * x[0] + self.w2 * x[1] + self.b1
-        h1 = sigmoid(sum_h1)
+        h1 = self.activation_f(sum_h1)
 
         sum_h2 = self.w3 * x[0] + self.w4 * x[1] + self.b2
-        h2 = sigmoid(sum_h2)
+        h2 = self.activation_f(sum_h2)
 
         sum_o1 = self.w5 * h1 + self.w6 * h2 + self.b3
-        o1 = sigmoid(sum_o1)
+        o1 = self.activation_f(sum_o1)
         y_pred = o1
 
         # --- Calculate partial derivatives.
@@ -73,22 +84,22 @@ class OurNeuralNetwork:
         d_L_d_ypred = -2 * (y_true - y_pred)
 
         # Neuron o1
-        d_ypred_d_w5 = h1 * deriv_sigmoid(sum_o1)
-        d_ypred_d_w6 = h2 * deriv_sigmoid(sum_o1)
-        d_ypred_d_b3 = deriv_sigmoid(sum_o1)
+        d_ypred_d_w5 = h1 * self.deriv_activation_f(sum_o1)
+        d_ypred_d_w6 = h2 * self.deriv_activation_f(sum_o1)
+        d_ypred_d_b3 = self.deriv_activation_f(sum_o1)
 
-        d_ypred_d_h1 = self.w5 * deriv_sigmoid(sum_o1)
-        d_ypred_d_h2 = self.w6 * deriv_sigmoid(sum_o1)
+        d_ypred_d_h1 = self.w5 * self.deriv_activation_f(sum_o1)
+        d_ypred_d_h2 = self.w6 * self.deriv_activation_f(sum_o1)
 
         # Neuron h1
-        d_h1_d_w1 = x[0] * deriv_sigmoid(sum_h1)
-        d_h1_d_w2 = x[1] * deriv_sigmoid(sum_h1)
-        d_h1_d_b1 = deriv_sigmoid(sum_h1)
+        d_h1_d_w1 = x[0] * self.deriv_activation_f(sum_h1)
+        d_h1_d_w2 = x[1] * self.deriv_activation_f(sum_h1)
+        d_h1_d_b1 = self.deriv_activation_f(sum_h1)
 
         # Neuron h2
-        d_h2_d_w3 = x[0] * deriv_sigmoid(sum_h2)
-        d_h2_d_w4 = x[1] * deriv_sigmoid(sum_h2)
-        d_h2_d_b2 = deriv_sigmoid(sum_h2)
+        d_h2_d_w3 = x[0] * self.deriv_activation_f(sum_h2)
+        d_h2_d_w4 = x[1] * self.deriv_activation_f(sum_h2)
+        d_h2_d_b2 = self.deriv_activation_f(sum_h2)
 
         # --- Update weights and biases
         # Neuron h1
@@ -126,12 +137,17 @@ all_y_trues = np.array([
   1, # Diana
 ])
 
-# Train our neural network!
-network = OurNeuralNetwork()
-network.train(data, all_y_trues)
+def main():
+    # Train our neural network!
+    network = OurNeuralNetwork(ReLu)
+    network.train(data, all_y_trues)
 
-# Make some predictions
-emily = np.array([-7, -3]) # 128 pounds, 63 inches
-frank = np.array([20, 2])  # 155 pounds, 68 inches
-print("Emily: %.3f" % network.feedforward(emily)) # 0.951 - F
-print("Frank: %.3f" % network.feedforward(frank)) # 0.039 - M
+    # Make some predictions
+    emily = np.array([-7, -3]) # 128 pounds, 63 inches
+    frank = np.array([20, 2])  # 155 pounds, 68 inches
+    print("Emily: %.3f" % network.feedforward(emily)) # 0.951 - F sigmoid   / 1 ReLu
+    print("Frank: %.3f" % network.feedforward(frank)) # 0.039 - M sigmoid   / 0 ReLu
+
+
+if __name__ == "__main__":
+    main()
